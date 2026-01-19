aff4835d73fbb67772c329c47005b8cae794c06d
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2026-01-19 21:17:15 +0900

    swift用の設定

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "sourcekit" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
