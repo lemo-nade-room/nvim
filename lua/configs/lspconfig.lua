d5440bea1612e8565673e36a387d2bee392bd1ed
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2026-02-26 21:14:01 +0900

    feat: ✨ NeovimのLSP対応を拡張してPKLを有効化

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,28 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "sourcekit" }
+local servers = {
+  "lua_ls",
+  "html",
+  "cssls",
+  "ts_ls",
+  "svelte",
+  "bashls",
+  "yamlls",
+  "jsonls",
+  "taplo",
+  "pkl_ls",
+  "terraformls",
+  "tflint",
+  "gh_actions_ls",
+  "sourcekit",
+}
+
+vim.lsp.config("pkl_ls", {
+  cmd = { "pkl-lsp" },
+  filetypes = { "pkl" },
+  root_markers = { "PklProject", ".git" },
+})
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,11 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.filetype.add {
+  extension = {
+    pkl = "pkl",
+  },
+}
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
